5173a8d33c324e4f2832463391e238f8c4227ba0
Ahmad Shahzaib <ahmadshahzaib-tekhqs>
2024-04-30 10:46:33 +0500

    added

--- src/mongoDb/schema/Demo.schema.ts
+++ src/mongoDb/schema/Demo.schema.ts
@@ -19,7 +19,7 @@ export const DemoSchema = new mongoose.Schema(
     email: { type: String, required: true, index: true },
     phoneNumber: { type: String },
     firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
+    lastName: { type: String },
     usdot: { type: String, required: true },
     companyName: { type: String, required: true },
     state: { type: String },
