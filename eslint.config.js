480f7b082b25f4a43071f523aba97dcec8114464
Ahmad Shahzaib <ahmadshahzaib-tekhqs>
2024-06-13 14:15:53 +0500

    added rule

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,12 @@ module.exports = [
       rules: {
           semi: "warn",
           // "prefer-const": "error"
-      }
-  }
+      }      
+  },
+  {
+    ignores: [
+        "dist/**/*",        // ignore all contents in and under `build/` directory but not the `build/` directory itself
+        
+    ]
+}
 ];
